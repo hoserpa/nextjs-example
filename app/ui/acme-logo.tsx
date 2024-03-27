efd3d5dda9ea6ef573001c99a775a07dda5b4d02
Jose Ramon Alvarez <joraalgo@gmail.com>
2024-03-27 10:18:55 +0100

    Learn complete

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,5 +1,6 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { useSession } from 'next-auth/react';
 
 export default function AcmeLogo() {
   return (
